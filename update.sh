9ba74fad6de84cad00c2b2395f6fc8c4cf676356
D.E.S.N_OFFICIAL17 <131302001+Saldarriaga184@users.noreply.github.com>
2024-03-24 19:34:04 -0500

    Actualizar update.sh

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 # Código desarrollado por @gata_dios 
   
 #!/data/data/com.termux/files/usr/bin/bash 
-BOT_DIR="GataBot-MD"  
-BOT_REPO="https://github.com/GataNina-Li/$BOT_DIR"
+BOT_DIR="KanakiBot-MD"  
+BOT_REPO="https://github.com/Saldarriaga184/$BOT_DIR"
 DB_FILE="database.json"
  
 GREEN='\033[32m'
