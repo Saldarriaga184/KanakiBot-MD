34778d99db153ff070a4c8e9b62bc198be1093a0
D.E.S.N_OFFICIAL17 <131302001+Saldarriaga184@users.noreply.github.com>
2024-05-16 09:44:14 -0500

    Actualizar sticker-telegram.js

--- plugins/sticker-telegram.js
+++ plugins/sticker-telegram.js
@@ -1,13 +1,13 @@
 import fetch from "node-fetch"
 import { sticker } from '../lib/sticker.js'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-if (!args[0]) throw `${lenguajeGB['smsAvisoIIG']()}𝙄𝙉𝙂𝙍𝙀𝙎𝙀 𝙀𝙇 𝙀𝙉𝙇𝘼𝘾𝙀 𝘿𝙀 𝙎𝙏𝙄𝘾𝙆𝙀𝙍 𝙏𝙀𝙇𝙀𝙂𝙍𝘼𝙈\n𝙀𝙅𝙀𝙈𝙋𝙇𝙊:\n${usedPrefix + command} https://t.me/addstickers/Porcientoreal\n\n𝙀𝙉𝙏𝙀𝙍 𝙏𝙃𝙀 𝙎𝙏𝙄𝘾𝙆𝙀𝙍 𝙏𝙀𝙇𝙀𝙂𝙍𝘼𝙈 𝙇𝙄𝙉𝙆\n𝙀𝙓𝘼𝙈𝙋𝙇𝙀:\n${usedPrefix + command} https://t.me/addstickers/Porcientoreal`
-if (!args[0].match(/(https:\/\/t.me\/addstickers\/)/gi)) throw `${lenguajeGB['smsAvisoIIG']()}𝙇𝘼 𝙐𝙍𝙇 𝙀𝙎 𝙄𝙉𝘾𝙊𝙍𝙍𝙀𝘾𝙏𝘼\n𝙏𝙃𝙀 𝙐𝙍𝙇 𝙄𝙎 𝙄𝙉𝘾𝙊𝙍𝙍𝙀𝘾𝙏`
+if (!args[0]) throw `${lenguajeGB['smsAvisoIIG']()}𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝙴𝙽𝙻𝙰𝙲𝙴 𝙳𝙴 𝚂𝚃𝙸𝙲𝙺𝙴𝚁 𝚃𝙴𝙻𝙴𝙶𝚁𝙰𝙼\n𝙴𝙹𝙴𝙼𝙿𝙻𝙾:\n${usedPrefix + command} https://t.me/addstickers/Porcientoreal\n\n𝙴𝙽𝚃𝙴𝚁 𝚃𝙷𝙴 𝚂𝚃𝙸𝙲𝙺𝙴𝚁 𝚃𝙴𝙻𝙴𝙶𝚁𝙰𝙼 𝙻𝙸𝙽𝙺\n𝙴𝚇𝙰𝙼𝙿𝙻𝙴:\n${usedPrefix + command} https://t.me/addstickers/Porcientoreal`
+if (!args[0].match(/(https:\/\/t.me\/addstickers\/)/gi)) throw `${lenguajeGB['smsAvisoIIG']()}𝙻𝙰 𝚄𝚁𝙻 𝙴𝚂 𝙸𝙽𝙲𝙾𝚁𝚁𝙴𝙲𝚃𝙰\n𝚃𝙷𝙴 𝚄𝚁𝙻 𝙸𝚂 𝙸𝙽𝙲𝙾𝚁𝚁𝙴𝙲𝚃`
 let packName = args[0].replace("https://t.me/addstickers/", "")
 let gas = await fetch(`https://api.telegram.org/bot891038791:AAHWB1dQd-vi0IbH2NjKYUk-hqQ8rQuzPD4/getStickerSet?name=${encodeURIComponent(packName)}`, { method: "GET", headers: { "User-Agent": "GoogleBot" } })
 if (!gas.ok) throw eror
 let json = await gas.json()
-m.reply(`${lenguajeGB['smsAvisoIIG']()} *𝙎𝙏𝙄𝘾𝙆𝙀𝙍 𝙏𝙊𝙏𝘼𝙇𝙀𝙎:* ${json.result.stickers.length}\n*𝙀𝙉𝙑𝙄𝘼𝘿𝙊 𝙀𝙇:* ${json.result.stickers.length * 1.5} Segundos`.trim())
+m.reply(`${lenguajeGB['smsAvisoIIG']()} *𝚂𝚃𝙸𝙲𝙺𝙴𝚁 𝚃𝙾𝚃𝙰𝙻𝙴𝚂:* ${json.result.stickers.length}\n*𝙴𝙽𝚅𝙸𝙰𝙳𝙾 𝙴𝙻:* ${json.result.stickers.length * 1.5} Segundos`.trim())
 for (let i = 0; i < json.result.stickers.length; i++) {
 let fileId = json.result.stickers[i].thumb.file_id
 let gasIn = await fetch(`https://api.telegram.org/bot891038791:AAHWB1dQd-vi0IbH2NjKYUk-hqQ8rQuzPD4/getFile?file_id=${fileId}`)
